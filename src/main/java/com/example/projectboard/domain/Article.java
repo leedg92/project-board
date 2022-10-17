7b49bf85da3a7d912a9c1fbe28e936be4afde245
leedg92 <49174093+leedg92@users.noreply.github.com>
2022-10-17 22:14:41 +0900

    20221017 object

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -4,13 +4,13 @@ import java.time.LocalDateTime;
 
 public class Article {
     private Long id;
-    private String content;
-    private String title;
-    private String hashtag;
+    private String title;   //제목
+    private String content; //본문
+    private String hashtag; //해시태그
 
     //metaData
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt;    //생성일시
+    private String createdBy;           //생성자
+    private LocalDateTime modifiedAt;   //수정일시
+    private String modifiedBy;          //수정자
 }
--- src/main/java/com/example/projectboard/domain/ArticleComment.java
+++ src/main/java/com/example/projectboard/domain/ArticleComment.java
@@ -5,13 +5,13 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
     private Long id;
-    private Article article;
-    private String comment;
+    private Article article;    //본문
+    private String comment;     //댓글
 
     //metaData
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt;    //생성일시
+    private String createdBy;           //생성자
+    private LocalDateTime modifiedAt;   //수정일시
+    private String modifiedBy;          //수정자
 
 }
