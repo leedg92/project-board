254698e46c22e968ef9394befb39c0295dff0df5
leedg92 <49174093+leedg92@users.noreply.github.com>
2022-11-02 20:31:40 +0900

    소스 덮어쓰기 커밋
    다른 경로에서 가져온 소스를 동작하는 소스로 덮어씀

---
+++ project-board/src/main/java/com/example/projectboard/domain/ArticleComment.java
@@ -0,0 +1,59 @@
+package com.example.projectboard.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "comment"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class ArticleComment extends AuditingFields{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @ManyToOne(optional = false) private Article article;    //본문
+    @Setter @Column(nullable = false, length = 500) private String comment;     //댓글
+
+
+
+    protected ArticleComment() {
+    }
+
+    public ArticleComment(Article article, String comment) {
+        this.article = article;
+        this.comment = comment;
+    }
+
+    public static ArticleComment of(Article article, String content){
+        return new ArticleComment(article,content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ project-board/src/main/java/com/example/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,10 @@
+package com.example.projectboard.repository;
+
+import com.example.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
+
